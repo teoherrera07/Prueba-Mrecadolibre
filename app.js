06630e0a75418e0651d3cce78ac1e892cd08d9d5
Mateo Herrera Tamayo <teoherrera07@gmail.com>
2023-09-24 11:47:31 -0500

    Upload

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+
+
+app.listen(process.env.PORT || 3000, function (){
+    console.log('Servidor funcionando');
+});
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/home.html');
+});
+
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
+
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
+
+app.post('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
